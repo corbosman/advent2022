8b8048ea3bd55fb1ae5850e401e95c55af9cd5e9
Cor Bosman <cor@xs4all.net>
2022-12-02 10:51:53 +0100

    wip

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,56 +1,9 @@
 <?php
 use function Termwind\{render};
 
-function output($str) : void
-{
-    print_r($str . "\n");
-}
-
 function input($path) : array
 {
     return file(realpath(dirname($_SERVER['PHP_SELF'])) . '/' .  $path, FILE_IGNORE_NEW_LINES);
-}
-
-
-function map(callable $callback, array $array, array ...$arrays): array
-{
-    return array_map($callback, $array, ...$arrays);
-}
-
-function sum($array) : int|float
-{
-    return array_sum($array);
-}
-
-function reduce(array $array, callable $callback, mixed $initial) : mixed
-{
-    return array_reduce($array, $callback, $initial);
-}
-
-function reverse(array $array) : array
-{
-    return array_reverse($array);
-}
-
-function merge(...$arrays) : array {
-    return array_merge(...$arrays);
-}
-
-function filter(array $array, callable $callback, $mode = 0) : array
-{
-    return array_filter($array, $callback, $mode);
-}
-
-function str_diff($str1, $str2) : string
-{
-    return strlen($str1) > strlen($str2) ?
-        implode('', array_values(array_diff(str_split($str1), str_split($str2)))) :
-        implode('', array_values(array_diff(str_split($str2), str_split($str1))));
-}
-
-function str_uniq($str) : string
-{
-    return implode('',array_unique(str_split($str)));
 }
 
 function render_output($solutions) : void
