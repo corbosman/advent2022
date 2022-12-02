8aaa02a455ec6993ec2721a30a995989665365b6
Cor Bosman <cor@xs4all.net>
2022-12-02 10:40:22 +0100

    wip

--- src/Advent02_RockPaperScissors.php
+++ src/Advent02_RockPaperScissors.php
@@ -13,13 +13,14 @@ class Advent02_RockPaperScissors extends Solver
             'C X' => 7, 'C Y' => 2, 'C Z' => 6
         });
 
+        $solutions->add('2a', $this->title(), $score_a->sum());
+
         $score_b = $input->map(fn($game) => match($game) {
             'A X' => 3, 'A Y' => 4, 'A Z' => 8,
             'B X' => 1, 'B Y' => 5, 'B Z' => 9,
             'C X' => 2, 'C Y' => 6, 'C Z' => 7
         });
 
-        $solutions->add('2a', $this->title(), $score_a->sum());
         $solutions->add('2b', $this->title(), $score_b->sum());
     }
 }
