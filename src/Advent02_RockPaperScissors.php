e59ad3a3c355aeeadd18f444326ca653e61a9df3
Cor Bosman <cor@xs4all.net>
2022-12-02 10:39:23 +0100

    wip

--- src/Advent02_RockPaperScissors.php
+++ src/Advent02_RockPaperScissors.php
@@ -8,17 +8,15 @@ class Advent02_RockPaperScissors extends Solver
     public function solve(Collection $input, Solutions $solutions) : void
     {
         $score_a = $input->map(fn($game) => match($game) {
-               'A X' => 4, 'A Y' => 8, 'A Z' => 3,
-               'B X' => 1, 'B Y' => 5, 'B Z' => 9,
-               'C X' => 7, 'C Y' => 2, 'C Z' => 6
-        );
+            'A X' => 4, 'A Y' => 8, 'A Z' => 3,
+            'B X' => 1, 'B Y' => 5, 'B Z' => 9,
+            'C X' => 7, 'C Y' => 2, 'C Z' => 6
+        });
 
-        $score_b = $input->map(function($game) use ($solutions) {
-            return match($game) {
-                'A X' => 3, 'A Y' => 4, 'A Z' => 8,
-                'B X' => 1, 'B Y' => 5, 'B Z' => 9,
-                'C X' => 2, 'C Y' => 6, 'C Z' => 7
-            };
+        $score_b = $input->map(fn($game) => match($game) {
+            'A X' => 3, 'A Y' => 4, 'A Z' => 8,
+            'B X' => 1, 'B Y' => 5, 'B Z' => 9,
+            'C X' => 2, 'C Y' => 6, 'C Z' => 7
         });
 
         $solutions->add('2a', $this->title(), $score_a->sum());
