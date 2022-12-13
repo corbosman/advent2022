f8e2460676079c3e718a75d5efdd454123193584
Cor Bosman <cor@xs4all.net>
2022-12-13 01:06:55 +0100

    remove debug

--- day11_monkey_in_the_middle/day11_monkey_in_the_middle.php
+++ day11_monkey_in_the_middle/day11_monkey_in_the_middle.php
@@ -35,7 +35,6 @@ class day11_monkey_in_the_middle extends solver
                     if ($rounds === 20) $item = (int)floor($item / 3);
 
                     $item %= $monkey->lcm;
-                    output($item);
 
                     /* now perform the actual test */
                     $test = ($item % $monkey->mod === 0) ? 'true' : 'false';
