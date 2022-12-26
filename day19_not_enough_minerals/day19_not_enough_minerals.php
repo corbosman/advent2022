892e58cee5a6ccb4174bf4d21dd28bf297ca6b27
Cor Bosman <cor@xs4all.net>
2022-12-27 00:20:14 +0100

    refactor

--- day19_not_enough_minerals/day19_not_enough_minerals.php
+++ day19_not_enough_minerals/day19_not_enough_minerals.php
@@ -14,7 +14,7 @@ class day19_not_enough_minerals extends solver
 
         $blueprints = $this->parse_input($this->input);
         $this->solution('19a', $this->part1($blueprints));
-        // $this->solution('19b', $this->part2($blueprints));
+        $this->solution('19b', $this->part2($blueprints));
 
         return $this->solutions;
     }
@@ -35,13 +35,15 @@ class day19_not_enough_minerals extends solver
         $max_geodes = 0;
         $cache = new Map;
         $queue = new Deque;
-        $queue->push([$max_time,0,0,0,0,1,0,0,0]);
+        $queue->push([$max_time, 0, [0,0,0,1,0,0,0]]);
+        $states = 1;
 
         while($queue->count() > 0) {
-            $state = $queue->shift();
+            $states++;
+            [$time, $geodes, $state] = $queue->shift();
 
-            if ($state[0] <= 0)  {
-                $max_geodes = max($max_geodes, $state[4]);
+            if ($time <= 0)  {
+                $max_geodes = max($max_geodes, $geodes);
                 continue;
             }
 
@@ -51,46 +53,46 @@ class day19_not_enough_minerals extends solver
             $cache->put($state, 1);
 
             if ($this->can_build_geode_robot($state, $bp)) {
-                $next_state = $this->mine($state);
+                $next_state = $this->mine($state, $bp);
                 $next_state = $this->build_geode_robot($next_state, $bp);
-                $queue->push($next_state);
+                $queue->push([$time-1, $geodes+$state[6], $next_state]);
                 continue;
             }
 
             if ($this->can_build_obsidian_robot($state, $bp)) {
-                $next_state = $this->mine($state);
+                $next_state = $this->mine($state, $bp);
                 $next_state = $this->build_obsidian_robot($next_state, $bp);
-                $queue->push($next_state);
+                $queue->push([$time-1, $geodes+$state[6], $next_state]);
+
             }
 
             if ($this->can_build_clay_robot($state, $bp)) {
-                $next_state = $this->mine($state);
+                $next_state = $this->mine($state, $bp);
                 $next_state = $this->build_clay_robot($next_state, $bp);
-                $queue->push($next_state);
+                $queue->push([$time-1, $geodes+$state[6], $next_state]);
             }
 
             if ($this->can_build_ore_robot($state, $bp)) {
-                $next_state = $this->mine($state);
+                $next_state = $this->mine($state, $bp);
                 $next_state = $this->build_ore_robot($next_state, $bp);
-                $queue->push($next_state);
+                $queue->push([$time-1, $geodes+$state[6], $next_state]);
             }
 
             /* dont build anything, just mine resources */
-            $state = $this->mine($state);
-            $queue->push($state);
+            $state = $this->mine($state, $bp);
+            $queue->push([$time-1, $geodes+$state[6], $state]);
         }
-        output($max_geodes);
+        output("geodes={$max_geodes} states={$states}");
         return $max_geodes;
     }
 
     /* 0=time 1=ore 2=clay 3=obsidian 4=geode 5=ore_robot 6=clay_robot 7=obsidian_robot 8=geode_robot */
-    public function mine(array $state) : array
+    public function mine(array $state, Blueprint $bp) : array
     {
-        $state[0]--;                    // subtract a minute
-        $state[1] += $state[5];         // mine ore
-        $state[2] += $state[6];         // mine clay
-        $state[3] += $state[7];         // mine obsidian
-        $state[4] += $state[8];         // mine geodes
+        $state[0] += $state[3];         // mine ore
+        $state[1] += $state[4];         // mine clay
+        $state[2] += $state[5];         // mine obsidian
+
         return $state;
     }
 
@@ -98,10 +100,10 @@ class day19_not_enough_minerals extends solver
     public function can_build_geode_robot(array $state, Blueprint $bp) : bool
     {
         /* not enough ore */
-        if ($state[1] < $bp->geode_robot_ore) return false;
+        if ($state[0] < $bp->geode_robot_ore) return false;
 
         /* not enough obsidian */
-        if ($state[3] < $bp->geode_robot_obsidian) return false;
+        if ($state[2] < $bp->geode_robot_obsidian) return false;
 
         return true;
     }
@@ -109,9 +111,9 @@ class day19_not_enough_minerals extends solver
     /* 0=time 1=ore 2=clay 3=obsidian 4=geode 5=ore_robot 6=clay_robot 7=obsidian_robot 8=geode_robot */
     private function build_geode_robot(array $state, Blueprint $bp): array
     {
-        $state[8]++;                                // geode robot
-        $state[1] -= $bp->geode_robot_ore;          // ore
-        $state[3] -= $bp->geode_robot_obsidian;     // obsidian
+        $state[6]++;                                // geode robot
+        $state[0] -= $bp->geode_robot_ore;          // ore
+        $state[2] -= $bp->geode_robot_obsidian;     // obsidian
         return $state;
     }
 
@@ -119,13 +121,13 @@ class day19_not_enough_minerals extends solver
     public function can_build_obsidian_robot(array $state, Blueprint $bp) : bool
     {
         /* not enough ore */
-        if($state[1] < $bp->obsidian_robot_ore) return false;
+        if($state[0] < $bp->obsidian_robot_ore) return false;
 
         /* not enough clay */
-        if($state[2] < $bp->obsidian_robot_clay) return false;
+        if($state[1] < $bp->obsidian_robot_clay) return false;
 
         /* dont build more obsidian robots than the max cost of the geode robot */
-        if ($state[7] >= $bp->max_obsidian_cost) return false;
+        if ($state[5] >= $bp->max_obsidian_cost) return false;
 
         return true;
     }
@@ -133,9 +135,9 @@ class day19_not_enough_minerals extends solver
     /* 0=time 1=ore 2=clay 3=obsidian 4=geode 5=ore_robot 6=clay_robot 7=obsidian_robot 8=geode_robot */
     public function build_obsidian_robot(array $state, Blueprint $bp) : array
     {
-        $state[7]++;                                // obsidian robot
-        $state[1] -= $bp->obsidian_robot_ore;       // ore
-        $state[2] -= $bp->obsidian_robot_clay;      // clay
+        $state[5]++;                                // obsidian robot
+        $state[0] -= $bp->obsidian_robot_ore;       // ore
+        $state[1] -= $bp->obsidian_robot_clay;      // clay
         return $state;
     }
 
@@ -143,10 +145,10 @@ class day19_not_enough_minerals extends solver
     public function can_build_clay_robot(array $state, Blueprint $bp) : bool
     {
         /* not enough ore */
-        if ($state[1] < $bp->clay_robot_ore) return false;
+        if ($state[0] < $bp->clay_robot_ore) return false;
 
         /* never build more clay robots than the max cost of the obsidian robot */
-        if ($state[6] >= $bp->max_clay_cost) return false;
+        if ($state[4] >= $bp->max_clay_cost) return false;
 
         return true;
     }
@@ -154,8 +156,8 @@ class day19_not_enough_minerals extends solver
     /* 0=time 1=ore 2=clay 3=obsidian 4=geode 5=ore_robot 6=clay_robot 7=obsidian_robot 8=geode_robot */
     public function build_clay_robot(array $state, Blueprint $bp) : array
     {
-        $state[6]++;                                // clay robot
-        $state[1] -= $bp->clay_robot_ore;           // ore
+        $state[4]++;                                // clay robot
+        $state[0] -= $bp->clay_robot_ore;           // ore
         return $state;
     }
 
@@ -163,10 +165,10 @@ class day19_not_enough_minerals extends solver
     public function can_build_ore_robot(array $state, Blueprint $bp) : bool
     {
         /* not enough ore */
-        if ($state[1] < $bp->ore_robot_ore) return false;
+        if ($state[0] < $bp->ore_robot_ore) return false;
 
         /* never build more ore robots than the largest ore cost */
-        if ($state[5] >= $bp->max_ore_cost) return false;
+        if ($state[3] >= $bp->max_ore_cost) return false;
 
         return true;
     }
@@ -174,8 +176,8 @@ class day19_not_enough_minerals extends solver
     /* 0=time 1=ore 2=clay 3=obsidian 4=geode 5=ore_robot 6=clay_robot 7=obsidian_robot 8=geode_robot */
     public function build_ore_robot(array $state, Blueprint $bp) : array
     {
-        $state[5]++;                                // ore robot
-        $state[1] -= $bp->ore_robot_ore;            // ore
+        $state[3]++;                                // ore robot
+        $state[0] -= $bp->ore_robot_ore;            // ore
         return $state;
     }
 
